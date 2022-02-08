896919c2056ffa4c7b9762deaf313e7f6f9b9296
Simon Dickenson <mtwangyugeng@gmail.com>
2022-02-07 20:56:10 -0500

    finished timeline component

---
+++ portfolio/src/assets/timeline/Timeline.js
@@ -0,0 +1,56 @@
+import { useMemo } from "react";
+import styles from "./Timeline.module.css"
+
+export default function Timeline({timeline}) {
+    const enrichedTimeline = useMemo(() => {
+        const finale = (
+            <>
+            {timeline.map((v,i) => {
+                const date = v.date.toLocaleDateString();
+                const text = v.text;
+                const link = v.link;
+                const tag = v.tag;
+                return <TimelineItem key={i} date={date} text={text} link={link} tag={tag}/>
+            })}
+            </>
+        ) 
+        return finale;
+    }
+    , [timeline]);
+    return (
+        <div className={styles.main}>
+            {enrichedTimeline}
+        </div>
+    );
+}
+
+const TimelineItem = ({date, text, link, tag}) => {
+    return (
+        <div className={styles.itemContainer}>
+            <span className={styles.circle} />
+            <span className={styles.pole} />
+            <div className={styles.item}>
+                <div className={styles.itemContent}>
+                    <div className={styles.cardTop}>
+                        <span className={styles.tag} style={{ background: tag.color }}>
+                            {tag.title}
+                        </span>
+                        <time className={styles.date}>{date}</time>
+                    </div>
+                    <p className={styles.text}>{text}</p>
+                    {link && (
+                        <a
+                            className={styles.link}
+                            href={link.url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            {link.text + "▶"}
+                        </a>
+                    )}
+                    <span className="circle" />
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ portfolio/src/assets/timeline/UserStory.md
@@ -0,0 +1,43 @@
+# Career timeline
+
+- Example usage: ```javescript
+    <Timeline timeline={[
+      {
+        date:new Date("1996-07-09"),
+        text:'Bob is born in Jacksville Florida. The best city of the USA.',
+        tag: {
+          title: 'twitter',
+          color: '#1DA1F2'
+        },
+        link: {
+          url: '#!',
+          text: 'Check it out on GitHub'
+        }
+      },
+        {
+          date:new Date("1996-07-09"),
+          text:'Bob is born in Jacksville Florida. The best city of the USA.',
+          tag: {
+            title: 'Github',
+            color: 'red'
+          },
+          link: {
+            url: '#!',
+            text: 'Check it out on GitHub'
+          }
+        }
+        ,
+        {
+          date:new Date("1996-07-09"),
+          text:'Bob is born in Jacksville Florida. The best city of the USA.',
+          tag: {
+            title: 'Github',
+            color: 'red'
+          },
+          link: {
+            url: '#!',
+            text: 'Check it out on GitHub'
+          }
+        }
+    ]} />
+    ```
